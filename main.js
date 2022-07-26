c26605349c0bf746f5b3b6001d44024c32a3a048
Aziza <azizadlittle@gmail.com>
2022-07-26 16:11:16 -0400

    addedd buttons to html

---
+++ main.js
@@ -0,0 +1,5 @@
+//to make the bones of this caluclator i'm thinking i might have to use a canvas like in the Build A Game project, use flexbox for the layout of buttons, use some event listeners for clicks, some functions to manipulate the inputs behind the scenes, and somehow return or console.log the result of our inputs to that one bar.
+// making the canvas (the actual space of the calculator)
+// make a span that the DOM would print into (**span-id-"results")
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
