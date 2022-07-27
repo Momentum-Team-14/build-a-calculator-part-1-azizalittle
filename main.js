cf698a1c91218442db0a52f1c66e4b136b7a6352
Aziza <azizadlittle@gmail.com>
2022-07-27 15:44:36 -0400

    added some javascript

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-//to make the bones of this caluclator i'm thinking i might have to use a canvas like in the Build A Game project, use flexbox for the layout of buttons, use some event listeners for clicks, some functions to manipulate the inputs behind the scenes, and somehow return or console.log the result of our inputs to that one bar.
-// making the canvas (the actual space of the calculator)
-// make a span that the DOM would print into (**span-id-"results")
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
+//make the values on the buttons appear in the display when the corresponding number is clicked
+// i need to make the "resultArea" hold/print every value of the buttons clicked
+
+//create a variable that links to the result in HTML
+let resultMessage = document.querySelector('#result');
+
+//create a variable that represents all buttons in the calculator
+let calcButton = document.querySelectorAll('.button');
+//make an event listener for whenever buttons are clicked, the inner HTML in "result" will be updated
+calcButton.addEventListener('click',function(){
+    let button = document.querySelectorAll('.button');
+        resultMessage.innerHTML = button.innerHTML;
+}
+
+
+//make functions to perform math operations based on the buttons pressed
+
+
+
+
+
+
+
+//make clicking the "=" sign result in the answer being shown in the display
