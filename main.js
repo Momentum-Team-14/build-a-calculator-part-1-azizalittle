35271c55f3cad56eb208355d66447b59f49d6c88
Aziza <azizadlittle@gmail.com>
2022-08-01 08:07:06 -0400

    changed JS to calculate

--- main.js
+++ main.js
@@ -2,22 +2,24 @@ console.log('javascript is linked!');
 
 let calcButtons = document.querySelectorAll('.button');
 console.log('Buttons added', calcButtons);
-const display = document.getElementById('#display');
+const display = document.getElementById('Display');
 console.log('display updated');
 //**all buttons have to have class button^^ */
 
+
 for (let button of calcButtons) {
     button.addEventListener('click', (Event) => {
-        if (Event.target.id !== 'equals') {
-            display.inneText += Event.target.id;
+        if (Event.target.id !== 'eval') {
+            display.innerText += Event.target.id;
             console.log(Event.target.id);
         } 
         if (Event.target.id === 'clear') {
             display.innerText = "";
             console.log("That's how you clear it!");
         }
-        if (Event.target.id === 'equals') {
-            let result = eval(display.innerText);
+        
+        if (Event.target.id === 'eval') {
+            let result = eval(display.innerHTML);
             display.innerText = result;
             console.log(result);
             console.log('equals clicked');
